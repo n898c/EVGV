cd86249580a3b35af5b7d8be21344cab8dbec7b8
Nahiyan Chowdhury <74080527+n898c@users.noreply.github.com>
2023-02-04 23:10:33 -0500

    allowed hosts

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2!odh+lg93-#aoqut0u(=e&*s@q&+r-z(16ue$t2*fbzkb7-vq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
